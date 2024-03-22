17884aefb6ead5a9725b36f1bbc8f739b2ce3695
Sophia Hage <sophia@sophiah.gay>
2024-03-22 20:04:05 +0100

    Quick control panel for cobblestone generator

---
+++ src/main.ts
@@ -0,0 +1,92 @@
+import { createServer } from "http";
+import { readFileSync } from "fs";
+import { WebSocket, WebSocketServer } from "ws";
+
+enum State {
+  UNKNOWN = "unknown",
+  OFF = "off",
+  ON = "on",
+}
+
+let cobblestoneState = State.UNKNOWN;
+let openWebSockets: WebSocket[] = [];
+
+const server = createServer();
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("close", () => {
+    openWebSockets.unshift(ws);
+    console.log("Connection closed");
+  });
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+    try {
+      const json = JSON.parse(data.toString());
+      if (json.type === "set") {
+        if (json.state === "on") {
+          cobblestoneState = State.ON;
+        } else if (json.state === "off") {
+          cobblestoneState = State.OFF;
+        } else {
+          console.warn("Unknown state: " + json.state);
+        }
+      }
+    } catch (e) {
+      console.warn("Could not parse message: " + data);
+    }
+  });
+
+  openWebSockets.push(ws);
+});
+
+server.listen(8080, () => {
+  console.log("Web socket server started on http://localhost:8080");
+});
+
+function turnOffCobblestoneGenerator() {
+  console.log("Turning off cobblestone generator");
+  openWebSockets.forEach((ws) =>
+    ws.send(JSON.stringify({ type: "set", state: "off" }))
+  );
+  cobblestoneState = State.OFF;
+}
+
+function turnOnCobblestoneGenerator() {
+  console.log("Turning on cobblestone generator");
+  openWebSockets.forEach((ws) =>
+    ws.send(JSON.stringify({ type: "set", state: "on" }))
+  );
+  cobblestoneState = State.ON;
+}
+
+// Create a http server
+import * as express from "express";
+import * as path from "path";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.post("/off", (_req, res) => {
+  turnOffCobblestoneGenerator();
+  res.send("OK");
+});
+
+app.post("/on", (_req, res) => {
+  turnOnCobblestoneGenerator();
+  res.send("OK");
+});
+
+app.get("/state", (_req, res) => {
+  res.send(cobblestoneState);
+});
+
+app.listen(8081, () => {
+  console.log("Web server started on http://localhost:8081");
+});
---
+++ src/startup.lua
@@ -0,0 +1,52 @@
+local myURL = "wss://happen-actress-gig-retail.trycloudflare.com"
+http.websocketAsync(myURL)
+local event, url, handle
+repeat
+	event, url, handle = os.pullEvent("websocket_success")
+until url == myURL
+
+print("Connected to websocket server")
+
+local State = {
+	OFF = "off",
+	ON = "on",
+}
+
+local state = State.OFF
+
+function update_server_state(new_state)
+	local message = {
+		type = "set",
+		state = new_state,
+	}
+	handle.send(textutils.serialiseJSON(message))
+end
+
+function turn_on_cobblestone_generator()
+	redstone.setOutput("top", true)
+	update_server_state(State.ON)
+end
+
+function turn_off_cobblestone_generator()
+	redstone.setOutput("top", false)
+	update_server_state(State.OFF)
+end
+
+turn_on_cobblestone_generator()
+
+while true do
+	local event, url, message
+	repeat
+		event, url, message = os.pullEvent("websocket_message")
+	until url == myURL
+
+	parsed = textutils.unserialiseJSON(message)
+
+	if parsed.type == "set" then
+		if parsed.state == State.ON then
+			turn_on_cobblestone_generator()
+		elseif parsed.state == State.OFF then
+			turn_off_cobblestone_generator()
+		end
+	end
+end
